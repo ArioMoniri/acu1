fe18f68ec854ffa923382fbe40147670d5640bb7
Ario Moniri <92126657+ArioMoniri@users.noreply.github.com>
2023-03-05 02:08:50 +0300

    Update acu1.py

--- acu1.py
+++ acu1.py
@@ -32,17 +32,29 @@ import streamlit as st
 
 def file_upload_widget(widget_id):
     return st.file_uploader("Upload a PDF file", type=["pdf"], key=widget_id)
+  
+import camelot
+import pandas as pd
+import tabula
+from tabula import read_pdf
 
 def main():
     global pdf_file1
     st.title("ACU")
     file_upload_id = "file_upload"
-
     pdf_file = st.file_uploader("Upload a PDF file", type=["pdf"], key=file_upload_id)
 
     if pdf_file is not None:
     # Read the uploaded file using PyPDF2
         pdf_file2 = PyPDF2.PdfReader(pdf_file)
+        tables = tabula.read_pdf(pdf_file2, pages='all',multiple_tables=True,stream=True, guess=True)
+
+    # loop through each table and save as CSV file
+        for i, table in enumerate(tables):
+            table.to_csv(f"table{i+1}.csv", index=False)
+
+    # concatenate all CSV files into a single DataFrame
+        datay = pd.concat([pd.read_csv(f"table{i+1}.csv") for i in range(len(tables))], ignore_index=True)
         pdf_file1 = 'f'
     
     else:
@@ -76,16 +88,16 @@ import pandas as pd
 import tabula
 from tabula import read_pdf
 
-if pdf_file1 is not None:
-    pdf_file2 = PyPDF2.PdfReader(pdf_file)
-    tables = tabula.read_pdf(pdf_file2, pages='all',multiple_tables=True,stream=True, guess=True)
+#if pdf_file1 is not None:
+    #pdf_file2 = PyPDF2.PdfReader(pdf_file)
+    #tables = tabula.read_pdf(pdf_file2, pages='all',multiple_tables=True,stream=True, guess=True)
 
     # loop through each table and save as CSV file
-    for i, table in enumerate(tables):
-        table.to_csv(f"table{i+1}.csv", index=False)
+    #for i, table in enumerate(tables):
+        #table.to_csv(f"table{i+1}.csv", index=False)
 
     # concatenate all CSV files into a single DataFrame
-    datay = pd.concat([pd.read_csv(f"table{i+1}.csv") for i in range(len(tables))], ignore_index=True)
+    #datay = pd.concat([pd.read_csv(f"table{i+1}.csv") for i in range(len(tables))], ignore_index=True)
         
     
 
