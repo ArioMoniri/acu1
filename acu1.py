c819aca0a1b6ec4570c3d6019b19e79c60b1b179
Ario Moniri <92126657+ArioMoniri@users.noreply.github.com>
2023-03-05 01:17:49 +0300

    Update acu1.py

--- acu1.py
+++ acu1.py
@@ -72,7 +72,7 @@ import pandas as pd
 # Convert the PDF to a DataFrame using the convert_pdf_to_csv function
 import camelot
 import pandas as pd
-
+tables = None
 if pdf_file is not None:
     tables = camelot.read_pdf(pdf_file)
     for i, table in enumerate(tables):
