6d9f9436f4998955dd7c1910654e6254e85b8138
Ario Moniri <92126657+ArioMoniri@users.noreply.github.com>
2023-03-06 20:41:02 +0300

    Update acu1.py

--- acu1.py
+++ acu1.py
@@ -38,6 +38,7 @@ import pdfplumber
 import csv
 import io
 import streamlit as st
+from io import StringIO
 
 def extract_data(feed):
 # Open the PDF file
@@ -77,7 +78,8 @@ if uploaded_file is not None:
     global pdf_file1
     pdf_file1 = 'd'
     df = extract_data(uploaded_file)
-    #df = pd.DataFrame(df)
+    StringData = StringIO(df)
+    df = pd.read_csv(StringData, sep =",")
     st.write(df)
     st.write(type(df))
 else:
