1ab5f40b15121f2544a3b5cefa07936eaeda3599
Ario Moniri <92126657+ArioMoniri@users.noreply.github.com>
2023-03-06 22:42:54 +0300

    Update acu1.py

--- acu1.py
+++ acu1.py
@@ -57,7 +57,7 @@ from pytesseract import Output, TesseractError
 import pdftables_api
 import io
 import tempfile
-conversion = pdftables_api.Client('iqe7dhe1adbm')
+conversion = pdftables_api.Client('66zlndzcwsgr')
 def extract_data(pdf_file):
     with tempfile.NamedTemporaryFile(delete=False) as temp:
         temp.write(pdf_file.read())
